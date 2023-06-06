4d2e0ecb8ba330bed565d834a89b9ee173abba31
Paulo Vanderley <vanderley.1109@gmail.com>
2023-06-06 14:53:12 -0300

    atualizando conteudo

--- main.c
+++ main.c
@@ -5,31 +5,56 @@
 void main()
 {
     setlocale(LC_ALL, "Portuguese");
-    int vetor[5];
-    int auxiliar;
+    int vetor1[5];
+    int vetor2[5];
+    int moda = 0;
+    int contador = 0;
 
     for (int i = 0; i < 5; i++)
     {
         printf("Digite um número para a posição %d do vetor: ", i + 1);
-        scanf("%d", &vetor[i]);
+        scanf("%d", &vetor1[i]);
     }
 
     for (int i = 0; i < 5; i++)
     {
-        for (int j = i; j < 5; j++)
+        vetor2[i] = 0;
+    }
+
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
         {
-            if (vetor[i] > vetor[j])
+            if (vetor1[i] == vetor1[j])
             {
-                auxiliar = vetor[i];
-                vetor[i] = vetor[j];
-                vetor[j] = auxiliar;
+                vetor2[i] = vetor2[i] + 1;
             }
         }
     }
 
     for (int i = 0; i < 5; i++)
     {
-        printf("Posição %d, índice %d, número armazenado: %d\n", i + 1, i, vetor[i]);
+        if (vetor2[i] > contador)
+        {
+            contador = vetor2[i];
+        }
+    }
+
+    for (int i = 0; i < 5; i++)
+    {
+        if (vetor2[i] == contador)
+        {
+            moda = i;
+        }
+    }
+
+    if (contador < 2)
+    {
+        printf("Nenhum número se repete");
+    }
+    else
+    {
+        printf("\nA moda dos números armazenados e: %d", vetor1[moda]);
     }
     return 0;
 }
